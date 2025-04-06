402787dcc4b5853cafe391b0f0009224ce271af4
Thomas Lindop <thomas16256@gmail.com>
2025-04-06 22:56:28 +0100

    >> remove last remains of config system

--- config/config.sh
+++
@@ -1,6 +0,0 @@
-# The extension executables that yona makes in single file mode
-EXE_EXT='.yonax'
-# The program to be used to print out (non-JSON) project command files.
-# Use 'bat --style plain' if you have bat
-CAT_PROVIDER=cat
-
